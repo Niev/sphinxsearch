b98ceed57ea2aaf0eeb9fd95588770bba2dbfb26
Niev <evselyev@yandex.ru>
2016-03-03 17:00:31 +0300

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 if [ $# -eq 0 ]; then
-  if ! [ -f "/var/lib/sphinxsearch/data/.INDEXED" ]; then
+  if ! [ -f "/var/lib/sphinxsearch/.INDEXED" ]; then
     echo "No indexes yet. Start indexing."
     indexer --rotate --all
-    touch /var/lib/sphinxsearch/data/.INDEXED
+    touch /var/lib/sphinxsearch/.INDEXED
   fi
   
 else
   indexer --rotate "$@"
 fi
 
-searchd --nodetach
+exec searchd --nodetach
